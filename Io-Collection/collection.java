f700e45ffa13956887b5126eb20b5efe38b04500
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-11-06 14:48:25 +0000

    Create collection.java

---
+++ Io-Collection/collection.java
@@ -0,0 +1,27 @@
+package person.java;
+
+public class collection {
+     private person[] People;
+
+     private static int MAX_SIZE = 200;
+
+     private int Size;
+
+     private int min;
+
+     private int max;
+
+     private int total;
+
+     private static String OBJFNAME = "";
+
+     public collection(int max){
+          People = new person(max);
+     }
+
+     public void readfromfile(String filename){
+
+     }
+}
+
+
