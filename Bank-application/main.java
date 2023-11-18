09ace8034bed5aebe3151715f27dee441e5d6967
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-11-18 23:32:28 +0000

    Update main.java

--- Bank-application/main.java
+++ Bank-application/main.java
@@ -2,18 +2,18 @@ package bankaccount;
 
 public class main {
     public static void main(String[] args) {
-        // Creating a new bank account
+        //Creating a new bank account
         BankAccount newaccount = new BankAccount("Abdul Mateen", 19064164,1234, 1000);
 
-        // Printing the account number and initial balance
+        //Printing the account number and initial balance
         System.out.println("Account Number: " + newaccount.getAccountnumber());
         System.out.println("Initial Balance: $" + newaccount.checkbalance());
 
-        // Attempting a withdrawal and deposit
+        //Attempting a withdrawal and deposit
         newaccount.withdrawMoney(100);
         newaccount.depositMoney(1000);
 
-        // Printing the updated balance
+        //Printing the updated balance
         System.out.println("Updated Balance after withdrawal and deposit: $" + newaccount.checkbalance());
 
         // Printing the account holder's name
