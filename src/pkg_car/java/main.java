a377965d62bd68868fc1670367c464f1a57136ec
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-11-21 23:56:48 +0000

    Update main.java

--- src/pkg_car/java/main.java
+++ src/pkg_car/java/main.java
@@ -1,10 +1,21 @@
+// Package declaration for the car class
 package pkg_car.java;
 
+
 public class main {
-    public static void main(String[] args) {
-        car Tesla = new car("ModelS", 23, 222);
-        System.out.println(Tesla.getModel());
-        System.out.println(Tesla.getTankSize());
-        System.out.println(Tesla.estimateDistance());
-    }
+
+  // Main method
+  public static void main(String[] args) {
+    // Create a new car object named "Tesla" with model "ModelS", fuel capacity 23 gallons, and MPG 222
+    car Tesla = new car("ModelS", 23, 222);
+
+    // Print the model of the car
+    System.out.println(Tesla.getModel());
+
+    // Print the fuel capacity of the car
+    System.out.println(Tesla.getTankSize());
+
+    // Print the estimated distance the car can travel with a full tank
+    System.out.println(Tesla.estimateDistance());
+  }
 }
