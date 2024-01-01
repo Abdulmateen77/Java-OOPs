3bfc4f4768256fa28c0d52fa3dca8a6efb73f6eb
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-01-01 23:43:49 +0000

    Update main.java

--- Bank-application/main.java
+++ Bank-application/main.java
@@ -22,5 +22,6 @@ public class main {
         //Depositing a large sum and printing the final balance
         newaccount.depositMoney(100000);
         System.out.println("Final Balance after a large deposit: $" + newaccount.checkbalance());
+        
     }
 }
